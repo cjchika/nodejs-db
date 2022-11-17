aa6949507ac07a65a2a4956f0072ae82106fe051
Chijioke Chika <8pluscreative@gmail.com>
2022-11-17 04:20:52 +0100

    Added Model

--- controllers/products.js
+++ controllers/products.js
@@ -1,14 +1,16 @@
-export const products = [];
+import { Product } from "../models/product.js";
 
 export const getAddProduct = (req, res, next) => {
   res.render("add-product", { pageTitle: "Add Product" });
 };
 
 export const postAddProduct = (req, res, next) => {
-  products.push({ title: req.body.title });
+  const product = new Product(req.body.title);
+  product.save();
   res.redirect("/");
 };
 
 export const getProducts = (req, res, next) => {
+  const products = Product.fetchAll();
   res.render("shop", { prods: products, pageTitle: "Shop" });
 };
