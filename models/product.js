1a65a07fab7829e654190a9fb77be3c27d314d97
Chijioke Chika <8pluscreative@gmail.com>
2022-11-17 05:01:18 +0100

    Added Model

--- models/product.js
+++ models/product.js
@@ -1,4 +1,9 @@
-const products = [];
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export class Product {
   constructor(title) {
@@ -6,7 +11,17 @@ export class Product {
   }
 
   save() {
-    products.push(this);
+    const prod = path.join(__dirname, "data", "products.json");
+    fs.readFile(prod, (err, fileContent) => {
+      let products = [];
+      if (!err) {
+        products = JSON.parse(fileContent);
+      }
+      products.push(this);
+      fs.writeFile(prod, JSON.stringify(products), (err) => {
+        console.log(err);
+      });
+    });
   }
 
   static fetchAll() {
