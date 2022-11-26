5b4e6a73f98e422477880eec58fd385586470c53
Chijioke Chika <8pluscreative@gmail.com>
2022-11-26 04:16:15 +0100

    Node-MongoDB

--- app.js
+++ app.js
@@ -2,13 +2,7 @@ import express from "express";
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
 
-import sequelize from "./utils/database.js";
-import { Product } from "./models/product.js";
-import { User } from "./models/user.js";
-import { Cart } from "./models/cart.js";
-import { CartItem } from "./models/cart-item.js";
-import { Order } from "./models/order.js";
-import { OrderItem } from "./models/order-item.js";
+import { mongoConnect } from "./utils/database.js";
 
 import path from "path";
 import { fileURLToPath } from "url";
@@ -23,57 +17,30 @@ app.set("views", "views");
 
 dotenv.config();
 
-import adminRoutes from "./routes/admin.js";
-import shopRoutes from "./routes/shop.js";
-import { get404Page } from "./controllers/error.js";
+// import adminRoutes from "./routes/admin.js";
+// import shopRoutes from "./routes/shop.js";
+// import { get404Page } from "./controllers/error.js";
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use((req, res, next) => {
-  User.findByPk(1)
-    .then((user) => {
-      req.user = user;
-      next();
-    })
-    .catch((err) => console.log(err));
+// app.use((req, res, next) => {
+//   User.findByPk(1)
+//     .then((user) => {
+//       req.user = user;
+//       next();
+//     })
+//     .catch((err) => console.log(err));
+// });
+
+// app.use("/admin", adminRoutes);
+// app.use(shopRoutes);
+
+// app.use(get404Page);
+
+const PORT = process.env.PORT || 5000;
+
+mongoConnect((client) => {
+  console.log(client);
+  app.listen(PORT, () => console.log(`Server runnning on port: ${PORT}`));
 });
-
-app.use("/admin", adminRoutes);
-app.use(shopRoutes);
-
-app.use(get404Page);
-
-Product.belongsTo(User, { constraints: true, onDelete: "CASCADE" });
-User.hasMany(Product);
-User.hasOne(Cart);
-Cart.belongsTo(User);
-Cart.belongsToMany(Product, { through: CartItem });
-Product.belongsToMany(Cart, { through: CartItem });
-Order.belongsTo(User);
-User.hasMany(Order);
-Order.belongsToMany(Product, { through: OrderItem });
-
-sequelize
-  // .sync({ force: true })
-  .sync()
-  .then((result) => {
-    return User.findByPk(1);
-  })
-  .then((user) => {
-    if (!user) {
-      User.create({ name: "Chika", email: "test@test.com" });
-    }
-    return user;
-  })
-  .then((user) => {
-    user.createCart();
-  })
-  .then((cart) => {
-    console.log(cart);
-    const PORT = process.env.PORT || 5000;
-    app.listen(PORT, () => console.log(`Server runnning on port: ${PORT}`));
-  })
-  .catch((err) => {
-    console.log(err);
-  });
--- utils/database.js
+++ utils/database.js
@@ -1,8 +1,15 @@
-import { Sequelize } from "sequelize";
+import mongodb from "mongodb";
+const MongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize("nodedb", "root", "password", {
-  dialect: "mysql",
-  host: "localhost",
-});
-
-export default sequelize;
+export const mongoConnect = (callback) => {
+  MongoClient.connect(
+    "mongodb+srv://mern_gee:chiefchief100@cluster0.l7yrx98.mongodb.net/?retryWrites=true&w=majority"
+  )
+    .then((client) => {
+      console.log("Bingo!");
+      callback(client);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
