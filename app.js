f79555a9969a617c25a7507616d93a3df765ad87
Chijioke Chika <8pluscreative@gmail.com>
2022-12-03 04:07:02 +0100

    Login

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ dotenv.config();
 
 import adminRoutes from "./routes/admin.js";
 import shopRoutes from "./routes/shop.js";
+import authRoutes from "./routes/auth.js";
 import { get404Page } from "./controllers/error.js";
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -37,6 +38,7 @@ app.use((req, res, next) => {
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
+app.use(authRoutes);
 
 app.use(get404Page);
 
